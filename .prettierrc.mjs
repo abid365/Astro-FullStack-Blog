e334ae235178b23b63e605daa95b37e77d6acf40
jawad Bin Azam <abidibnazam@gmail.com>
2023-11-10 23:20:22 +0600

    finished part -3

--- .prettierrc.mjs
+++ .prettierrc.mjs
@@ -1,5 +1,13 @@
 // .prettierrc.mjs
 /** @type {import("prettier").Config} */
 export default {
-  plugins: ['prettier-plugin-astro'],
+  plugins: ["prettier-plugin-astro"],
+  overrides: [
+    {
+      files: "*.astro",
+      options: {
+        parser: "astro",
+      },
+    },
+  ],
 };
